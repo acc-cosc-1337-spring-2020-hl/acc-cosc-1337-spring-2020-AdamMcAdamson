49dedadf30cae218f160ec2e674a75eb96e3dd8b
AdamMcAdamson <adam.brunn@g.austincc.edu>
2020-02-03 18:42:28 -0600

    if and boolean expressions

--- src/examples/02_module/01_if/if.cpp
+++ src/examples/02_module/01_if/if.cpp
@@ -1,11 +1,20 @@
 //write include statement for if header
-
+#include"if.h"
 
 
 //write code for the is_even function that returns true if num is even, false otherwise
+bool is_even(int num)
+{
+	return num % 2 == 0; //num % 2 == 0 is a boolean expression
+//	return (num+1) % 2;  //out == 0 == false, out > 0 == true
+}
 
 
 //write code for function named overtime that accepts a double hours and returns true if 
 //hours over 40, false otherwise
+bool overtime(double num)
+{
+	return false;
+}
 
 
