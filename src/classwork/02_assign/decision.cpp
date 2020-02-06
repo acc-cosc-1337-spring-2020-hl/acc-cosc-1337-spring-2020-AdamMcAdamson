96ff57c6cae180e577774bfdf4c706b4dcaa54bd
AdamMcAdamson <adam.brunn@g.austincc.edu>
2020-02-05 18:46:08 -0600

    Class work completed

--- src/classwork/02_assign/decision.cpp
+++ src/classwork/02_assign/decision.cpp
@@ -1,1 +1,19 @@
+#include "decision.h"
 //Write the function code that returns the product of hours and hourly_rate.
+
+double gross_pay(double hours, double hourly_rate)
+{
+	double total = 0.0;
+	
+	if (hours > 40) {
+		total = 40 * hourly_rate + ((hours-40)*hourly_rate*1.5);
+	}
+	else {
+		total = hourly_rate * hours;
+	}
+
+	int value = (int)(total * 100 + .5);
+	total = (double)value / 100;
+
+	return total;
+}
