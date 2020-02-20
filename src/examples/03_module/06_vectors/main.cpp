1028dbad76e7af1f5cde898425d41953bfe8614e
AdamMcAdamson <adam.brunn@g.austincc.edu>
2020-02-19 19:13:01 -0600

    vector type function examples

--- src/examples/03_module/06_vectors/main.cpp
+++ src/examples/03_module/06_vectors/main.cpp
@@ -4,7 +4,16 @@ int main()
 {//9, 10, 99, 5, 67
 	vector<int> nums{9, 10, 99, 5, 67};
 
-	loop_vector_w_index(nums);
+	//loop_vector_w_index(nums);
+	//loop_vector_w_index_ref(nums);
+
+	vector<int> num1;
+	num1.push_back(9);
+	
+	vector<int> num2(5, 10);
+	cout << num2.at(2) << "\n";
+	cout << num2.size() << "\n";
+	cout << num2.max_size() << "\n";
 
 	return 0;
 }
